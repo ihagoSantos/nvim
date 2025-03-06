d71a6f0796b465ea94b66521bfdb95d87a4f27ee
ihagoSantos <ihagog12@gmail.com>
2025-03-06 07:35:23 -0300

    none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,10 @@ return {
 		-- setup for treesitter
 		  local config = require("nvim-treesitter.configs")
 		config.setup({
-		  ensure_installed = {"lua", "javascript", "query", "html", "csv", "css", "json", "python", "typescript", "yaml"}, -- ensure that the parsers for lua and javascript are installed
+		  -- ensure_installed = {"lua", "javascript", "query", "html", "csv", "css", "json", "python", "typescript", "yaml"}, -- ensure that the parsers for lua and javascript are installed
+          
+          auto_install = true, -- automatically install when treesitter encounters a new language
+
 		  highlight = { enable = true }, -- taks a lua table to configure highlighting
 		  ident = { enable = true }
 		})
