4f73afeb67de4dd6f08882301384eed920201f5b
ihagoSantos <ihagog12@gmail.com>
2025-03-07 07:43:35 -0300

    add completions.lua config + update lsp-config.lua to add cmp_nvim_lsp default capabilities

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,46 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function ()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            -- install lsp to lua
-            lspconfig.lua_ls.setup({})
-            -- install plp to tsserver
-            lspconfig.ts_ls.setup({})
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- require cmp_nvim_lsp thats is installed on completions.lua file
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			-- install lsp to lua
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            -- Display docs of function in lua pressing <shift> + k
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            -- go to definition
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            -- code_actions
-            vim.keymap.set({ 'n' },  '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+
+			-- install lsp to tsserver
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+
+			-- Display docs of function in lua pressing <shift> + k
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			-- go to definition
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			-- go to references
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			-- code_actions
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
