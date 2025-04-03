bb3ca5da08e3fa2d5afe7dc52628cf5a9489c9c0
ihagoSantos <ihagog12@gmail.com>
2025-04-03 20:29:58 -0300

    add config to toggle neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,25 @@
 return {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- não estritamente necessário, mas recomendado
-        "MunifTanjim/nui.nvim"
-      },
-      config = function () 
-            vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>', {}) -- <crtl> + b -> open Neotree on the left side
-      end     
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- mostrar arquivos ocultos
+                    hide_dotfiles = false, -- não ocultar arquivos que começam com '.'
+                    hide_gitignored = false, -- não ocultar arquivos ignorados pelo gitignore
+                },
+            },
+        })
+
+        -- <crtl> + b -> open Neotree on the left side
+        -- vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>",{})
+        -- <ctrl> + b -> open/close Neotree
+        vim.keymap.set("n", "<C-b>", ":Neotree toggle<CR>", { noremap = true, silent = true })
+    end,
 }
